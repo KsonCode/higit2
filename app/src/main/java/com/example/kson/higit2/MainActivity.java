1bea2e0464ebe4fb2e255c079183e8672788f813
KsonCode <19655910@qq.com>
2018-03-09 17:20:14 +0800

    develop

--- app/src/main/java/com/example/kson/higit2/MainActivity.java
+++ app/src/main/java/com/example/kson/higit2/MainActivity.java
@@ -9,9 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //develop完善代码
-        //develop
-        //提交新代码
 
 
     }
